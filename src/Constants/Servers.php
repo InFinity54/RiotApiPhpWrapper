c2c00b507a50fdffd3ca87f789ec1b41c38ad27d
InFinity_54 <corentinkreicher@gmail.com>
2023-03-14 18:29:10 +0100

    Adding translations for servers names

--- src/Constants/Servers.php
+++ src/Constants/Servers.php
@@ -6,82 +6,130 @@ class Servers
 {
     public static array $br = [
         "id" => "BR1",
-        "name" => "Brésil",
+        "name" => [
+            "en" => "Brazil",
+            "fr" => "Brésil"
+        ],
         "shortName" => "BR"
     ];
     public static array $eune = [
         "id" => "EUN1",
-        "name" => "Europe (Nord/Est)",
+        "name" => [
+            "en" => "Europe West",
+            "fr" => "Europe (Nord/Est)"
+        ],
         "shortName" => "EUNE"
     ];
     public static array $euw = [
         "id" => "EUW1",
-        "name" => "Europe (Ouest)",
+        "name" => [
+            "en" => "Europe West",
+            "fr" => "Europe (Ouest)"
+        ],
         "shortName" => "EUW"
     ];
     public static array $jp = [
         "id" => "JP1",
-        "name" => "Japon",
+        "name" => [
+            "en" => "Japan",
+            "fr" => "Japon"
+        ],
         "shortName" => "JP"
     ];
     public static array $kr = [
         "id" => "KR",
-        "name" => "Corée du Sud",
+        "name" => [
+            "en" => "South Korea",
+            "fr" => "Corée du Sud"
+        ],
         "shortName" => "KR"
     ];
     public static array $lan = [
         "id" => "LA1",
-        "name" => "Amérique Latine (Nord)",
+        "name" => [
+            "en" => "Latin America North",
+            "fr" => "Amérique Latine (Nord)"
+        ],
         "shortName" => "LAN"
     ];
     public static array $las = [
         "id" => "LA2",
-        "name" => "Amérique Latine (Sud)",
+        "name" => [
+            "en" => "Latin America South",
+            "fr" => "Amérique Latine (Sud)"
+        ],
         "shortName" => "LAS"
     ];
     public static array $na = [
         "id" => "NA1",
-        "name" => "Amérique du Nord",
+        "name" => [
+            "en" => "North America",
+            "fr" => "Amérique du Nord"
+        ],
         "shortName" => "NA"
     ];
     public static array $oce = [
         "id" => "OC1",
-        "name" => "Océanie",
+        "name" => [
+            "en" => "Oceania",
+            "fr" => "Océanie"
+        ],
         "shortName" => "OCE"
     ];
     public static array $ph = [
         "id" => "PH2",
-        "name" => "Philippines",
+        "name" => [
+            "en" => "Philippines",
+            "fr" => "Philippines"
+        ],
         "shortName" => "PH"
     ];
     public static array $ru = [
         "id" => "RU",
-        "name" => "Russie",
+        "name" => [
+            "en" => "Russia",
+            "fr" => "Russie"
+        ],
         "shortName" => "RU"
     ];
     public static array $sg = [
         "id" => "SG2",
-        "name" => "Singapour, Malaisie & Indonésie",
+        "name" => [
+            "en" => "Singapore, Malaysia & Indonesia",
+            "fr" => "Singapour, Malaisie & Indonésie"
+        ],
         "shortName" => "SG"
     ];
     public static array $th = [
         "id" => "TH2",
-        "name" => "Thaïlande",
+        "name" => [
+            "en" => "Thailand",
+            "fr" => "Thaïlande"
+        ],
         "shortName" => "TH"
     ];
     public static array $tr = [
         "id" => "TR1",
-        "name" => "Turquie",
+        "name" => [
+            "en" => "Turkey",
+            "fr" => "Turquie"
+        ],
         "shortName" => "TR"
     ];
     public static array $tw = [
         "id" => "TW2",
-        "name" => "Taïwan, Hong Kong & Macao",
+        "name" => [
+            "en" => "Taiwan, Hong Kong & Macau",
+            "fr" => "Taïwan, Hong Kong & Macao"
+        ],
         "shortName" => "TW"
     ];
     public static array $vn = [
         "id" => "VN2",
-        "name" => "Vietnam",
+        "name" => [
+            "en" => "Vietnam",
+            "fr" => "Vietnam"
+        ],
         "shortName" => "VN"
     ];
 
